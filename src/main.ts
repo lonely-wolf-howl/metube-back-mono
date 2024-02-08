919d8cf3ba8a1f45e1f33a19039038066daaabdd
kme-developer <electruc0095@gmail.com>
2024-02-08 20:18:08 +0900

    build. default setting.

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,38 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import {
+  DocumentBuilder,
+  SwaggerCustomOptions,
+  SwaggerModule,
+} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = 3000;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  // Swagger
+  const config = new DocumentBuilder()
+    .setTitle('NestJS project')
+    .setDescription('NestJS project API description')
+    .setVersion('1.0')
+    .addBearerAuth()
+    .build();
+  const customOptions: SwaggerCustomOptions = {
+    swaggerOptions: {
+      persistAuthorization: true,
+    },
+  };
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document, customOptions);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+    })
+  );
+
+  await app.listen(port);
+  console.info(`listening on port ${port}`);
 }
 bootstrap();
