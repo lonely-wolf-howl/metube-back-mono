14a5e008a63e235b120664ea97ca075fca6c6177
kme-developer <electruc0095@gmail.com>
2024-02-11 12:53:36 +0900

    build. auth 모듈, 컨트롤러, 서비스 생성. test. ping 라우터 확인으로 서버 돌아가는 것 확인.

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,7 @@ import {
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = 3000;
+  const port = 4000;
   const app = await NestFactory.create(AppModule);
 
   // Swagger
