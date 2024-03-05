05ca388f0c150f58299d818767928568ac82cdba
kme-developer <electruc0095@gmail.com>
2024-03-05 16:44:51 +0900

    feat. ci/cd test

--- src/config/postgres.config.ts
+++ src/config/postgres.config.ts
@@ -1,9 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('postgres', () => ({
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  database: process.env.POSTGRES_DATABASE,
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: Number(process.env.POSTGRES_PORT) || 5433,
+  database: process.env.POSTGRES_DATABASE || 'metube',
+  username: process.env.POSTGRES_USERNAME || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
 }));
--- src/ormconfig.ts
+++ src/ormconfig.ts
@@ -2,11 +2,11 @@ import { DataSource } from 'typeorm';
 
 export const AppDateSource = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST,
-  port: Number(process.env.POSTGRES_PORT),
-  database: process.env.POSTGRES_DATABASE,
-  username: process.env.POSTGRES_USERNAME,
-  password: process.env.POSTGRES_PASSWORD,
+  host: process.env.POSTGRES_HOST || 'localhost',
+  port: Number(process.env.POSTGRES_PORT) || 5433,
+  database: process.env.POSTGRES_DATABASE || 'metube',
+  username: process.env.POSTGRES_USERNAME || 'postgres',
+  password: process.env.POSTGRES_PASSWORD || 'postgres',
   entities: [__dirname + '/**/*.entity{.ts,.js}'],
   synchronize: false,
 });
